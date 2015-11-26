89aa1770044f753ad984b11b60b695bc136fb8a7
cmillstein <cmillstein@fullsail.edu>
2015-11-25 22:19:39 -0500

    update

--- millstein_fundamentals/app/src/main/java/com/example/caseymillstein/millstein_fundamentals/MainActivity.java
+++ millstein_fundamentals/app/src/main/java/com/example/caseymillstein/millstein_fundamentals/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.caseymillstein.millstein_fundamentals;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     private ListView zipCodeSearch;
 
 
+    public static final String TAG = "MainActivity.TAG";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +30,14 @@ public class MainActivity extends AppCompatActivity {
         zipCode = (EditText) findViewById(R.id.zipCode);
         searchButton = (Button) findViewById(R.id.searchButton);
         zipCodeSearch = (ListView) findViewById(R.id.zipCodeList);
+
+        if(savedInstanceState == null){
+            WeatherListFragment frag = WeatherListFragment.newInstance();
+
+            getFragmentManager().beginTransaction().replace(R.id.fragment_container, frag, WeatherListFragment.TAG).commit();
+
+        }
+
 
 
     }
