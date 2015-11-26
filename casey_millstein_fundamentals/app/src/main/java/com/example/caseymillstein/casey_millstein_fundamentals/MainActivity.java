efcc3adb5fb386e58c22db9a742ce5c5a36c7d17
cmillstein <cmillstein@fullsail.edu>
2015-11-25 22:46:01 -0500

    commit

---
+++ casey_millstein_fundamentals/app/src/main/java/com/example/caseymillstein/casey_millstein_fundamentals/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.caseymillstein.casey_millstein_fundamentals;
+
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.app.Fragment;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private EditText zipCode;
+    private Button searchButton;
+    private ListView zipCodeSearch;
+
+
+    public static final String TAG = "MainActivity.TAG";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        zipCode = (EditText) findViewById(R.id.zipCode);
+        searchButton = (Button) findViewById(R.id.searchButton);
+        zipCodeSearch = (ListView) findViewById(R.id.zipCodeList);
+
+//        if(savedInstanceState == null){
+//            WeatherListFragment frag = WeatherListFragment.newInstance();
+//
+//            getFragmentManager().beginTransaction().replace(R.id.fragment_container, frag, WeatherListFragment.TAG).commit();
+//
+//        }
+
+
+
+    }
+
+
+
+//
+//    //CONNECTION
+//    protected boolean isOnline() {
+//
+//        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+//        NetworkInfo network = manager.getActiveNetworkInfo();
+//        return network != null && network.isConnectedOrConnecting();
+//
+//    }
+
+
+}
---
+++ casey_millstein_fundamentals/app/src/main/java/com/example/caseymillstein/casey_millstein_fundamentals/WeatherListFragment.java
@@ -0,0 +1,39 @@
+package com.example.caseymillstein.casey_millstein_fundamentals;
+
+
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+/**
+ * Created by caseymillstein on 11/25/15.
+ */
+public class WeatherListFragment extends ListFragment {
+
+    public static final String TAG = "WeatherListFragment.TAG";
+
+    public static WeatherListFragment newInstance() {
+        WeatherListFragment frag = new WeatherListFragment();
+        return frag;
+    }
+
+
+    public WeatherListFragment(){
+        super();
+    }
+
+//    @Override
+//    public void onListItemClick(ListView l, View v, int position, long id) {
+//        super.onListItemClick(l, v, position, id);
+//    }
+//
+//    @Override
+//    public void onActivityCreated(Bundle savedInstanceState) {
+//        super.onActivityCreated(savedInstanceState);
+//
+//        //GET INFO FROM API TO PUT INTO LIST
+//
+//        //CREATE ADAPTER FOR LIST
+//    }
+}
