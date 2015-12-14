51cc9e9527b575a9a43654162971827472c63b15
cmillstein <cmillstein@fullsail.edu>
2015-12-13 20:02:02 -0500

    Add Last Project

---
+++ c_millstein_labfive/app/src/main/java/com/example/caseymillstein/c_millstein_labfive/ListScreen.java
@@ -0,0 +1,13 @@
+package com.example.caseymillstein.c_millstein_labfive;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_screen);
+    }
+}
