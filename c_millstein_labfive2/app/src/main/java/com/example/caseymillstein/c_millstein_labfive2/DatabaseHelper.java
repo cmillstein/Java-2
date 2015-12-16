e51b5617a1aec785cc7125948fec7d4be211eb58
cmillstein <cmillstein@fullsail.edu>
2015-12-16 14:32:07 -0500

    Lots of updates

--- c_millstein_labfive2/app/src/main/java/com/example/caseymillstein/c_millstein_labfive2/DatabaseHelper.java
+++ c_millstein_labfive2/app/src/main/java/com/example/caseymillstein/c_millstein_labfive2/DatabaseHelper.java
@@ -7,51 +7,50 @@ import android.database.sqlite.SQLiteOpenHelper;
 /**
  * Created by caseymillstein on 12/16/15.
  */
-//public class DatabaseHelper extends SQLiteOpenHelper {
-//
-//
-//
-//
-//    //CONSTANTS
-//    private static final String DATABASE_NAME = "Database.db";
-//    private static final int DATABASE_VERSION = 1;
-//
-//
-//    public static final String STUDENT_INFO = "student";
-//    public static final String STUDENT_ID = "_id";
-//    public static final String NAME = "name";
-//    public static final String AGE = "age";
-//    public static final String SCHOOL = "school";
-//
-//    //creating SQL table
-//    private static final String TABLE_CREATE =
-//            "CREATE TABLE " + STUDENT_INFO + " (" +
-//                    STUDENT_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-//                    NAME + " TEXT, " +
-//                    AGE + " TEXT, " +
-//                    SCHOOL + " TEXT, " +
-//                    ")";
-//
-//
-//
-//    public DatabaseHelper (Context context) {
-//        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-//    }
-//
-//    //CREATING DATABASE
-//    @Override
-//    public void onCreate(SQLiteDatabase db) {
-//        db.execSQL(TABLE_CREATE);
-//
-//    }
-//
-//    //CHANGE DATABASE VERSION
-//    @Override
-//    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-//        db.execSQL("DROP TABLE IF EXISTS " + STUDENT_INFO);
-//        onCreate(db);
-//    }
-//
-//
-//
-//}
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+
+
+
+    //CONSTANTS
+    private static final String DATABASE_NAME = "Database.db";
+    private static final int DATABASE_VERSION = 1;
+
+
+    public static final String TABLE_NAME = "student";
+    public static final String STUDENT_ID = "_id";
+    public static final String NAME = "name";
+    public static final String AGE = "age";
+    public static final String SCHOOL = "school";
+
+    //creating SQL table
+    private static final String TABLE_CREATE =
+            "CREATE TABLE " + TABLE_NAME + "(" +
+                    STUDENT_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    NAME + " TEXT, " +
+                    AGE + " INTEGER, " +
+                    SCHOOL + " TEXT)";
+
+
+
+    public DatabaseHelper (Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    //CREATING DATABASE
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(TABLE_CREATE);
+
+    }
+
+    //CHANGE DATABASE VERSION
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+
+
+}
--- c_millstein_labfive2/app/src/main/java/com/example/caseymillstein/c_millstein_labfive2/NameProvider.java
+++ c_millstein_labfive2/app/src/main/java/com/example/caseymillstein/c_millstein_labfive2/NameProvider.java
@@ -2,44 +2,167 @@ package com.example.caseymillstein.c_millstein_labfive2;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
+
+import java.net.URI;
 
 /**
  * Created by caseymillstein on 12/16/15.
  */
 public class NameProvider extends ContentProvider {
+
+    private static final String AUTHORITY = "com.example.caseymillstein.c_millstein_labfive2.NameProvider";
+    private static final String DATA_SOURCE = "student";
+    public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + DATA_SOURCE);
+    public static final int PERSON = 1;
+    public static final int PERSON_ID = 2;
+    private DatabaseHelper myDatabase;
+
+    private static final UriMatcher URIMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+    static{
+        URIMatcher.addURI(AUTHORITY, DATA_SOURCE, PERSON);
+        URIMatcher.addURI(AUTHORITY, DATA_SOURCE + "/#", PERSON_ID);
+    }
+
+
+
     @Override
     public boolean onCreate() {
-        return false;
+        myDatabase = new DatabaseHelper(getContext());
+
+
+
+        return true;
     }
 
-    @Nullable
+
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
-        return null;
+
+        SQLiteQueryBuilder qBuilder = new SQLiteQueryBuilder();
+        qBuilder.setTables(DatabaseHelper.TABLE_NAME);
+
+        int uriType = URIMatcher.match(uri);
+        switch(uriType){
+            case PERSON_ID:
+                qBuilder.appendWhere(DatabaseHelper.STUDENT_ID + "=" + uri.getLastPathSegment());
+                break;
+
+            case PERSON:
+                break;
+
+            default:
+                throw new IllegalArgumentException("URI Not Valid");
+
+        }
+
+        Cursor newCursor = qBuilder.query(myDatabase.getReadableDatabase(), projection, selection, selectionArgs, null, null, sortOrder);
+        newCursor.setNotificationUri(getContext().getContentResolver(), uri);
+
+
+        return newCursor;
     }
 
-    @Nullable
+
     @Override
     public String getType(Uri uri) {
         return null;
     }
 
-    @Nullable
+
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        return null;
+        int type = URIMatcher.match(uri);
+        SQLiteDatabase sqdb = myDatabase.getWritableDatabase();
+
+        long id;
+        switch(type){
+            case PERSON:
+                id = sqdb.insert(DatabaseHelper.TABLE_NAME, null, values);
+                break;
+
+            default:
+                throw new IllegalArgumentException("URI Not Valid: " + uri);
+
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+
+
+
+        return Uri.parse(DATA_SOURCE + "/" + id);
     }
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        return 0;
+
+        int type = URIMatcher.match(uri);
+        SQLiteDatabase sqdb = myDatabase.getWritableDatabase();
+        int deleteRows;
+
+        switch(type){
+            case PERSON_ID:
+                String id = uri.getLastPathSegment();
+                if(TextUtils.isEmpty(selection)){
+                    deleteRows = sqdb.delete(DatabaseHelper.TABLE_NAME, DatabaseHelper.STUDENT_ID + "=" + id, null);
+                }else{
+                    deleteRows = sqdb.delete(DatabaseHelper.TABLE_NAME, DatabaseHelper.STUDENT_ID + "=" + id + "and" + selection, selectionArgs);
+                }
+                break;
+
+            default:
+                throw new IllegalArgumentException("URI Not Valid: " + uri);
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+
+
+
+        return deleteRows;
     }
 
     @Override
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        return 0;
+        int type = URIMatcher.match(uri);
+        SQLiteDatabase sqdb = myDatabase.getWritableDatabase();
+        int updateRows;
+
+        switch(type){
+            case PERSON:
+                updateRows = sqdb.update(DatabaseHelper.TABLE_NAME, values, selection, selectionArgs);
+                break;
+
+            case PERSON_ID:
+                String id = uri.getLastPathSegment();
+                if(TextUtils.isEmpty(selection)){
+                    updateRows = sqdb.update(DatabaseHelper.TABLE_NAME, values, DatabaseHelper.STUDENT_ID + "=" + id, null);
+
+                }else{
+                    updateRows = sqdb.update(DatabaseHelper.TABLE_NAME, values, DatabaseHelper.STUDENT_ID + "=" + id + "and" + selection, selectionArgs);
+
+                }
+                break;
+
+            default:
+                throw new IllegalArgumentException("URI Not Valid: " + uri);
+
+
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+
+
+
+
+        
+
+        return updateRows;
+
     }
 }
