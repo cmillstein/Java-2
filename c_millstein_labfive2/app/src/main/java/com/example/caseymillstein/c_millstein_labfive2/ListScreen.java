3be0e327249b8166dceec2ebebe7890fc6bcc980
cmillstein <cmillstein@fullsail.edu>
2015-12-16 13:57:41 -0500

    Updates

---
+++ c_millstein_labfive2/app/src/main/java/com/example/caseymillstein/c_millstein_labfive2/ListScreen.java
@@ -0,0 +1,27 @@
+package com.example.caseymillstein.c_millstein_labfive2;
+
+import android.app.ListFragment;
+
+import java.util.List;
+
+/**
+ * Created by caseymillstein on 12/16/15.
+ */
+public class ListScreen extends ListFragment {
+
+    public static final String TAG = "ListScreen.TAG";
+
+
+
+    public static ListScreen newInstance(){
+        ListScreen fragment = new ListScreen();
+        return fragment;
+    }
+
+
+
+
+
+
+
+}
